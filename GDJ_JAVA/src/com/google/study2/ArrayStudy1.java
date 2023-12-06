a1edd3e0a0cf89a570d9fa58f39b62dd8827f60a
양문석 <eun0343kr@naver.com>
2023-12-06 16:49:16 +0900

    add1

---
+++ GDJ_JAVA/src/com/google/study2/ArrayStudy1.java
@@ -0,0 +1,31 @@
+package com.google.study2;
+
+import java.util.Scanner;
+
+public class ArrayStudy1 {
+	public static void main(String[]args) {
+//		배
+//		변수는 값을 하나를 사용
+//		하나의 변수로 여러개의 값을 사용
+//		같은타입 데이터만 모을수있다.
+//		자바에서는 한번 선언한 배열의 크기는 조정x
+//		배열선언
+//		변수선언 공식 => 데이터 타입 변수명
+//		모을려고 하는 데이터타입 [] 변수명;
+		int num=0;
+		int [] ar = new int[10];
+		int ar2[] = new int [4];
+		String [] name = new String[4];
+		System.out.println(num);
+		String[]names = new String[4];
+		Scanner sc = new Scanner(System.in);
+//		for(int i=0;i<names.length;i++) {
+//			name[i]=sc.next();		}
+		System.out.println();
+//		1.메서드영역 2.스택영역(변수들이 차지하는공간)(배열의 주소가 들어있다.) 3. 힙영역(new)
+		
+		
+	}
+	
+
+}
